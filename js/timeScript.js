9cc99df425dc2df39bd8e0d7944c40c0baf51859
ole <s92933@bht-berlin.de>
2024-12-24 00:48:27 +0100

    refactored

--- js/timeScript.js
+++ js/timeScript.js
@@ -1,2 +1,8 @@
 const getTime = () => new Date().toTimeString()
 const getFormattedTime = () => console.log(`The time is ${getTime()}`)
+function textfeldAendern() {
+    // Das Textfeld per ID auswählen und den Wert ändern
+    let temp = Number(document.getElementById("specificTextField").textContent);
+    temp++;
+    document.getElementById("specificTextField").textContent = temp;
+}
