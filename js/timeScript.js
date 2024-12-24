cf8b675789d2b88af16d2beae5980b7abc23ee52
ole <s92933@bht-berlin.de>
2024-12-24 01:19:42 +0100

    random champ generation

--- js/timeScript.js
+++ js/timeScript.js
@@ -6,3 +6,22 @@ function textfeldAendern() {
     temp++;
     document.getElementById("specificTextField").textContent = temp;
 }
+
+let champs;
+
+async function initializeChamps() {
+    champs = await getAllChampsJSON();
+}
+async function getAllChampsJSON() {
+    let championData = await fetch("https://ddragon.leagueoflegends.com/cdn/14.22.1/data/en_US/champion.json")
+    return championData.json()
+}
+async function getAllChamps() {
+    console.log(Object.keys(champs.data).length)
+}
+async function getRandomChampion() {
+    let randomInt = Math.floor(Math.random() * Object.keys(champs.data).length)
+    document.getElementById("championTextField").textContent = "Champion: " + champs["data"][Object.keys(champs.data)[randomInt]]["name"];
+}
+
+initializeChamps();
